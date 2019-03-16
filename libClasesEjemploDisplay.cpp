9e3815582a551e3314e253552ab1f248f85e1e48
Alexander Calderon <calderonperaza@gmail.com>
2019-03-16 09:41:25 -0600

    archivos en blanco

---
+++ libClasesEjemploDisplay.cpp
@@ -0,0 +1,15 @@
+/* Archivo CPP incluye la definicion de todos los metodos
+definidos en el archivo de .H
+*/
+
+#include "Arduino.h"
+#include "libClasesEjemploDisplay.h"
+
+/* Ejemplo de un constructor de una clase
+int Clase::Metodo(int variableX)
+{
+  pinMode(variableX, OUTPUT);  
+  _variable = variableX;
+  return _variable;
+}
+*/
---
+++ libClasesEjemploDisplay.h
@@ -0,0 +1,16 @@
+/* ARCHIVO HEADER O .H 
+Este archivo contendra la estructura de todas las clases de la libreria
+*/
+
+#ifndef libClasesEjemploDisplay_h   // Definicion de la biblioteca de clases
+#define libClasesEjemploDisplay_h   // lleva el ifndef para no incluirse dos veces
+#include "Arduino.h"
+
+class display{  //Se define la clase display recuerden ; al cierre de la llave
+private:
+
+public:
+
+};
+
+#endif
